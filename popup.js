7531728051bdedb90a0c38e37a3dd4c4f30cdb3b
Jawad Hameed <jawadhameedlhr@gmail.com>
2018-03-06 16:03:32 +0500

    Revert "code formatting"
    This reverts commit d4312bed3df0d9b0a5af2b791e11f07272eca778.

--- popup.js
+++ popup.js
@@ -9,67 +9,66 @@
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-    // Query filter to be passed to chrome.tabs.query - see
-    // https://developer.chrome.com/extensions/tabs#method-query
-    var queryInfo = {
-        active: true,
-        currentWindow: true
-    };
+	// Query filter to be passed to chrome.tabs.query - see
+	// https://developer.chrome.com/extensions/tabs#method-query
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
 
-    chrome.tabs.query(queryInfo, (tabs) = > {
-        // chrome.tabs.query invokes the callback with a list of tabs that match the
-        // query. When the popup is opened, there is certainly a window and at least
-        // one tab, so we can safely assume that |tabs| is a non-empty array.
-        // A window can only have one active tab at a time, so the array consists of
-        // exactly one tab.
-        var tab = tabs[0];
+	chrome.tabs.query(queryInfo, (tabs) => {
+		// chrome.tabs.query invokes the callback with a list of tabs that match the
+		// query. When the popup is opened, there is certainly a window and at least
+		// one tab, so we can safely assume that |tabs| is a non-empty array.
+		// A window can only have one active tab at a time, so the array consists of
+		// exactly one tab.
+		var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+		// A tab is a plain object that provides information about the tab.
+		// See https://developer.chrome.com/extensions/tabs#type-Tab
+		var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+		// tab.url is only available if the "activeTab" permission is declared.
+		// If you want to see the URL of other tabs (e.g. after removing active:true
+		// from |queryInfo|), then the "tabs" permission is required to see their
+		// "url" properties.
+		console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-})
-    ;
+		callback(url);
+	});
 
-    // Most methods of the Chrome extension APIs are asynchronous. This means that
-    // you CANNOT do something like this:
-    //
-    // var url;
-    // chrome.tabs.query(queryInfo, (tabs) => {
-    //   url = tabs[0].url;
-    // });
-    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+	// Most methods of the Chrome extension APIs are asynchronous. This means that
+	// you CANNOT do something like this:
+	//
+	// var url;
+	// chrome.tabs.query(queryInfo, (tabs) => {
+	//   url = tabs[0].url;
+	// });
+	// alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
 function addKeepNote(result) {
-    /* chrome.tabs.executeScript({
-    file: 'script.js'
-    }); */
+	/* chrome.tabs.executeScript({
+	file: 'script.js'
+	}); */
 
-    chrome.storage.local.set({
-        result: result
-    }, function () {
-        chrome.tabs.executeScript({
-            file: 'script.js'
-        });
-    });
+	chrome.storage.local.set({
+		result: result
+	}, function () {
+		chrome.tabs.executeScript({
+			file: 'script.js'
+		});
+	});
 }
 
 function readEntries(entries) {
-    var entryLength = entries.length;
-    for (i = 0; i < entryLength; i++) {
-        var entry = entries[i];
-        var fileName = entry.filename.substring(entry.filename.lastIndexOf("/") + 1); //if inside folder
-        var ext = fileName.split(".").pop().toLowerCase();
-        alert(fileName + "===" + ext);
-    }
+	var entryLength = entries.length;
+	for (i = 0; i < entryLength; i++) {
+		var entry = entries[i];
+		var fileName = entry.filename.substring(entry.filename.lastIndexOf("/") + 1); //if inside folder
+		var ext = fileName.split(".").pop().toLowerCase();
+		alert(fileName + "===" + ext);
+	}
 }
 
 // This extension loads the saved background color for the current tab if one
@@ -80,49 +79,44 @@ function readEntries(entries) {
 // to a document's origin. Also, using chrome.storage.sync instead of
 // chrome.storage.local allows the extension data to be synced across multiple
 // user devices.
-document.addEventListener('DOMContentLoaded', () = > {
-    getCurrentTabUrl((url) =
->
-{
-    zip.workerScriptsPath = "/lib/";
-    var fileSelector = document.getElementById('file_select');
+document.addEventListener('DOMContentLoaded', () => {
+	getCurrentTabUrl((url) => {
+		zip.workerScriptsPath = "/lib/";
+		var fileSelector = document.getElementById('file_select');
 
-    fileSelector.onchange = function () {
-        var file = fileSelector.files[0];
+		fileSelector.onchange = function () {
+			var file = fileSelector.files[0];
 
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            // alert(e.target.result);
-            // addKeepNote(e.target.result);
-        };
-        reader.readAsText(file);
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				// alert(e.target.result);
+				// addKeepNote(e.target.result);
+			};
+			reader.readAsText(file);
 
-        // use a BlobReader to read the zip from a Blob object
-        zip.createReader(new zip.BlobReader(file), function (reader) {
-            // get all entries from the zip
-            reader.getEntries(function (entries) {
-                if (entries.length) {
-                    readEntries(entries);
-                    // get first entry content as text
-                    entries[0].getData(new zip.TextWriter(), function (text) {
-                        // text contains the entry data as a String
-                        alert(text);
-                        // close the zip reader
-                        reader.close(function () {
-                            // onclose callback
-                        });
+			// use a BlobReader to read the zip from a Blob object
+			zip.createReader(new zip.BlobReader(file), function (reader) {
+				// get all entries from the zip
+				reader.getEntries(function (entries) {
+					if (entries.length) {
+						readEntries(entries);
+						// get first entry content as text
+						entries[0].getData(new zip.TextWriter(), function (text) {
+							// text contains the entry data as a String
+							alert(text);
+							// close the zip reader
+							reader.close(function () {
+								// onclose callback
+							});
 
-                    }, function (current, total) {
-                        // onprogress callback
-                    });
-                }
-            });
-        }, function (error) {
-            // onerror callback
-        });
-    };
-}
-)
-;
-})
-;
+						}, function (current, total) {
+							// onprogress callback
+						});
+					}
+				});
+			}, function (error) {
+				// onerror callback
+			});
+		};
+	});
+});

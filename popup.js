76048bb1eac27c4581892112d1b199ad1a4082e7
Jawad Hameed <jawadhameedlhr@gmail.com>
2018-03-11 03:14:00 +0500

    remove comments

--- popup.js
+++ popup.js
@@ -115,14 +115,6 @@ function extractNoteData(entry) {
     });
 }
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
 document.addEventListener('DOMContentLoaded', () => {
     getCurrentTabUrl((url) => {
         zip.workerScriptsPath = "/lib/";
